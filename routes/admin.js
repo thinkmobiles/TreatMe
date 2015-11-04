1f5c92207b2d1d0283448a9f22d5d25969f891c8
Killer57575 <Killer57575@gmail.com>
2015-11-04 11:36:01 +0200

    GET clientList - with sorting

--- routes/admin.js
+++ routes/admin.js
@@ -25,11 +25,21 @@ module.exports = function(db){
     router.post('/stylist/approve/', sessionHandler.authenticatedUser, sessionHandler.isAdmin, admin.approveStylist);
     router.post('/stylist/suspend/', sessionHandler.authenticatedUser, sessionHandler.isAdmin, admin.suspendStylists);
 
+    // CRUD Clients
+    router.get('/client', sessionHandler.authenticatedUser, sessionHandler.isAdmin, admin.getClientList);
+    router.get('/client/:id', sessionHandler.authenticatedUser, sessionHandler.isAdmin, admin.getClientById);
+    router.put('/client', sessionHandler.authenticatedUser, sessionHandler.isAdmin, admin.updateClient);
+    router.post('/client', sessionHandler.authenticatedUser, sessionHandler.isAdmin, admin.createClient);
+    router.delete('/client', sessionHandler.authenticatedUser, sessionHandler.isAdmin, admin.removeClient);
+
+    router.post('/client/suspend', sessionHandler.authenticatedUser, sessionHandler.isAdmin, admin.suspendClient);
+    router.post('/client/activate', sessionHandler.authenticatedUser, sessionHandler.isAdmin, admin.activateClient);
+
     router.post('/appointments', sessionHandler.authenticatedUser, sessionHandler.isAdmin, admin.bookAppointment);
     router.put('/appointments', sessionHandler.authenticatedUser, sessionHandler.isAdmin, admin.suspendAppointments);
     router.delete('/appointments', sessionHandler.authenticatedUser, sessionHandler.isAdmin, admin.removeAppointments);
 
-    // CRUD Subscriptions
+    // CRUD SubscriptionType
     router.get('/subscriptionType', sessionHandler.authenticatedUser, sessionHandler.isAdmin, admin.getSubscriptionType);
     router.post('/subscriptionType', sessionHandler.authenticatedUser, sessionHandler.isAdmin, admin.addSubscriptionType);
     router.put('/subscriptionType/:id', sessionHandler.authenticatedUser, sessionHandler.isAdmin, admin.updateSubscriptionType);
--- routes/clients.js
+++ routes/clients.js
@@ -7,36 +7,12 @@ module.exports = function(app, db){
     var clientsHandler = new ClientsHandler(app, db);
     var sessionHandler = new SessionHandler();
 
-    /*router.post('/signIn', clientsHandler.signIn);
-    router.post('/signUp', clientsHandler.signUp);
-    router.get('/signOut', clientsHandler.signOut);
-
-    router.post('/forgotPassword', clientsHandler.forgotPassword);
-    router.get('/confirm/:token', clientsHandler.confirmRegistration);
-
-    router.get('/passwordChange/:forgotToken', clientsHandler.confirmForgotPass);
-    router.post('/passwordChange/:forgotToken', clientsHandler.changePassword);*/
-
-    //router.get('/gallery', sessionHandler.authenticatedUser, sessionHandler.isClient, clientsHandler.getGalleryPhotoes);
     router.get('/subscriptions', sessionHandler.authenticatedUser, sessionHandler.isClient, clientsHandler.getActiveSubscriptions);
-    //router.get('/appointment', sessionHandler.authenticatedUser, clientsHandler.getAllClientAppointments);
-    //router.get('/appointment/:id', sessionHandler.authenticatedUser, clientsHandler.getClientAppointmentById);
-
-    //router.get('/:id?', sessionHandler.authenticatedUser, sessionHandler.isClient, clientsHandler.getProfile);
-
-    //router.put('/coordinates', sessionHandler.authenticatedUser, sessionHandler.isClient, clientsHandler.updateLocation);
-
-    //router.put('/', sessionHandler.authenticatedUser, sessionHandler.isClient, clientsHandler.updateProfile);
-
-    //router.post('/avatar', sessionHandler.authenticatedUser, sessionHandler.isClient, clientsHandler.uploadAvatar);
-    //router.delete('/avatar', sessionHandler.authenticatedUser, sessionHandler.isClient, clientsHandler.removeAvatar);
 
     router.post('/appointment', sessionHandler.authenticatedUser, sessionHandler.clientOrAdmin, clientsHandler.createAppointment);
-    //router.post('/appointment/cancel', sessionHandler.authenticatedUser, clientsHandler.cancelByClient);
     router.post('/appointment/rate', sessionHandler.authenticatedUser, sessionHandler.isClient, clientsHandler.rateAppointmentById);
-    router.post('/gallery', sessionHandler.authenticatedUser, sessionHandler.clientOrAdmin, clientsHandler.addPhotoToGallery);
 
-    //router.delete('/gallery/:id', sessionHandler.authenticatedUser, sessionHandler.isClient, clientsHandler.removePhotoFromGallery);
+    router.post('/gallery', sessionHandler.authenticatedUser, sessionHandler.clientOrAdmin, clientsHandler.addPhotoToGallery);
 
     return router;
 };
