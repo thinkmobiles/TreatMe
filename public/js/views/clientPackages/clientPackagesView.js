f5dbc1bde0c1a3e2f62aae5a80cf9d5a0ef6d101
nazarovits <istvan.nazarovits@gmail.com>
2015-11-13 11:08:36 +0100

    client packages - suspend

--- public/js/views/clientPackages/clientPackagesView.js
+++ public/js/views/clientPackages/clientPackagesView.js
@@ -17,12 +17,52 @@ define([
 
         events: _.extend({
             //put events here ...
+            'click #removeSelectedBtn': 'deleteAllPackages',
+            'click .deleteCurrentBtn': 'deleteCurrentPackage'
         }, ListView.prototype.events),
 
         initialize: function (options) {
             App.Breadcrumbs.reset([{name: 'Client Packages', path: '#clientPackages'}]);
 
             ListView.prototype.initialize.call(this, options);
+        },
+
+        deleteAllPackages: function (e) {
+            var ids = this.getSelectedIds();
+
+            this.deletePackages(ids);
+        },
+
+        deleteCurrentPackage: function (e) {
+            var target = $(e.target);
+            var itemId = target.closest('tr').data('id');
+            var ids = [itemId];
+
+            this.deletePackages(ids);
+        },
+
+        deletePackages: function (ids) {
+            var data = {
+                packagesArray: ids
+            };
+            var dataStr = JSON.stringify(data);
+            var self = this;
+
+            $.ajax({
+                type: 'DELETE',
+                dataType: 'json',
+                contentType: 'application/json',
+                url: 'admin/packages',
+                data: dataStr,
+                success: function () {
+                    var params = self.pageParams;
+                    var page = params.page;
+                    var fetchParams = _.extend({reset: true}, params);
+
+                    self.collection.getPage(page, fetchParams); //fetch and re-render
+                },
+                error: self.handleErrorResponse //TODO
+            });
         }
     });
 
